ebb21562b449f17ee3887ac8b5d4c08c7fce6cbd
Daphnee <devdaphnee2@gmail.com>
2026-07-13 11:46:17 +0100

    Ajout de l'interface des commandes

--- glotelho-livreur/android/app/build.gradle.kts
+++ glotelho-livreur/android/app/build.gradle.kts
@@ -1,4 +1,4 @@
-plugins {
+/*plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
@@ -42,4 +42,103 @@ android {
 
 flutter {
     source = "../.."
+}*/
+
+/*plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
+
+android {
+    namespace = "com.example.glotelho_livreur"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    ccompileOptions {
+        // En Kotlin DSL moderne, il faut utiliser la fonction isCoreLibraryDesugaringEnabled
+        isCoreLibraryDesugaringEnabled = true
+
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        // Correction de la dépréciation pour jvmTarget
+        jvmTarget = "17"
+    }
+
+    defaultConfig {
+        applicationId = "com.example.glotelho_livreur"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+
+        // Sécurité supplémentaire si l'application devient trop lourde avec le desugaring
+        multiDexEnabled = true
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+// ATTENTION : Ce bloc doit être tout en bas, EN DEHORS du bloc android { }
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+}*/
+
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.glotelho_livreur"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    defaultConfig {
+        applicationId = "com.example.glotelho_livreur"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.2.2")
+}
+
+flutter {
+    source = "../.."
+}
+
