382a87d30dd72d9bc9aa59713e86866737e11df9
TCHINDA MEITANG Junior <meitangj@gmail.com>
2026-07-15 10:11:48 +0100

    Configuration environnement

--- glotelho-backoffice/backend/src/server.js
+++ glotelho-backoffice/backend/src/server.js
@@ -1,9 +1,24 @@
 require('dotenv').config();
+const os  = require('os');
 const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 
+// Récupère automatiquement l'IP locale de la machine
+function getLocalIP() {
+    const interfaces = os.networkInterfaces();
+    for (const name of Object.keys(interfaces)) {
+        for (const iface of interfaces[name]) {
+            if (iface.family === 'IPv4' && !iface.internal) {
+                return iface.address;
+            }
+        }
+    }
+    return 'localhost';
+}
+
 app.listen(PORT, '0.0.0.0', () => {
+    const ip = getLocalIP();
     console.log('Serveur Glotelho lance sur http://localhost:' + PORT);
-    console.log('Accessible sur le reseau : http://192.168.1.145:' + PORT);
+    console.log('Accessible sur le reseau : http://' + ip + ':' + PORT);
 });
